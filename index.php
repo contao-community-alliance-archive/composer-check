25656364bfec5df90d9764059aca3fbf7e4af181
Tristan Lins <tristan.lins@bit3.de>
2014-03-17 10:14:48 +0100

    Add phpdoc comment.

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * System Check for the Contao Composer Client
+ *
+ * PHP Version 5.3
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    composer-check
+ * @license    LGPL-3.0+
+ * @link       http://c-c-a.org
+ */
+
 if (!class_exists('Runtime')) {
 	require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 }
