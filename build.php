7e56913b49cbf7da05aaaf229674f2eee1b8465d
Tristan Lins <tristan.lins@bit3.de>
2014-03-17 08:24:17 +0100

    Improve PHAR build script.

--- build.php
+++ build.php
@@ -16,15 +16,15 @@ class build
 {
 	public function run()
 	{
-		$phar = new Phar('composer-check.phar');
+		$phar = new Phar('composer-check.phar', null, 'composer-check.phar');
 		$phar->setSignatureAlgorithm(\Phar::SHA1);
 		$phar->startBuffering();
 
-		$this->add($phar, __DIR__ . DIRECTORY_SEPARATOR . 'assets');
-		$this->add($phar, __DIR__ . DIRECTORY_SEPARATOR . 'src');
-		$this->add($phar, __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php');
-		$this->add($phar, __DIR__ . DIRECTORY_SEPARATOR . 'console.php');
-		$this->add($phar, __DIR__ . DIRECTORY_SEPARATOR . 'index.php');
+		$this->add($phar, 'assets');
+		$this->add($phar, 'src');
+		$this->add($phar, 'bootstrap.php');
+		$this->add($phar, 'console.php');
+		$this->add($phar, 'index.php');
 
 		$phar->setStub(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'stub.php'));
 		$phar->stopBuffering();
@@ -36,21 +36,19 @@ class build
 	 * @param Phar   $phar
 	 * @param string $path
 	 */
-	protected function add(Phar $phar, $path)
+	protected function add(Phar $phar, $relative)
 	{
-		$relative = str_replace(__DIR__ . DIRECTORY_SEPARATOR, '', $path);
+		$path = __DIR__ . DIRECTORY_SEPARATOR . $relative;
 
 		if (is_dir($path)) {
-			$phar->addEmptyDir($relative);
-
 			foreach (scandir($path) as $child) {
 				if ($child[0] != '.') {
-					$this->add($phar, $path . DIRECTORY_SEPARATOR . $child);
+					$this->add($phar, $relative . DIRECTORY_SEPARATOR . $child);
 				}
 			}
 		}
 		else if (is_file($path)) {
-			$phar->addFile($path, $relative);
+			$phar->addFromString($relative, file_get_contents($path));
 		}
 	}
 }
